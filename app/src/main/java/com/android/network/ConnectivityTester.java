2f783ee617c8ec69961b6cc01f15a6a347972649
Isuranga Perera <isurangamperera@gmail.com>
2017-02-20 23:51:19 +0530

    connectivity tester

---
+++ app/src/main/java/com/android/network/ConnectivityTester.java
@@ -0,0 +1,31 @@
+package com.android.network;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+/**
+ * Created by isurangaperera on 2/20/17.
+ */
+public class ConnectivityTester implements NetworkManagerInterface {
+    private Context context;
+
+    public ConnectivityTester(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public boolean isConnectedToInternet() {
+        ConnectivityManager connectivity =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if(info != null)
+                for(int i = 0; i < info.length; i++) {
+                    if(info[i].getState() == NetworkInfo.State.CONNECTED)
+                        return true;
+                }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/android/network/NetworkManager.java
+++
@@ -1,8 +0,0 @@
-package com.android.network;
-
-
-/**
- * Created by isurangaperera on 2/20/17.
- */
-public interface NetworkManager {
-}
