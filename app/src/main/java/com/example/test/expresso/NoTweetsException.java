57facba556a74c2a7e7dc40f358cb48698111aae
Isuranga Perera <isurangamperera@gmail.com>
2017-02-22 21:25:52 +0530

    exception class for NullTweetList

---
+++ app/src/main/java/com/example/test/expresso/NoTweetsException.java
@@ -0,0 +1,10 @@
+package com.example.test.expresso;
+
+/**
+ * Created by isurangaperera on 2/22/17.
+ */
+public class NoTweetsException extends Exception{
+    public NoTweetsException(String message) {
+        super(message);
+    }
+}
